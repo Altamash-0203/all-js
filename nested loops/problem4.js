9dd3720da2723b24f3635bf3e4950851b8fd065a
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-01 19:45:24 +0530

    nested example

---
+++ nested loops/problem4.js
@@ -0,0 +1,11 @@
+//father is expecting son is complate 5 set
+//1 set =10 round
+
+for (father=1;father<=5;father++)
+    console.log("_____set start_____")
+    {
+   for(son=1;son<=10;son++){
+    console.log("round no-",son)
+   }
+   console.log("_____set end_____")
+    }
