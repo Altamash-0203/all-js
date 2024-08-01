bc3323870c04b239140de3d6328f117e776985d6
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-01 20:21:19 +0530

    nested loop example

---
+++ nested loops/problem5.js
@@ -0,0 +1,10 @@
+//right angel tringle pattern
+for(i=1;i<=10;i++)
+    {
+        let pattern=""
+        for(j=1;j<=i;j++)
+            {
+                pattern=pattern+"x"
+            }
+     console.log(pattern)
+    }
