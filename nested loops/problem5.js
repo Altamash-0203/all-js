8b2a8817c93b18c5408366325e7d9eeeae34aed5
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-01 20:44:56 +0530

    nested loop exmaple

--- nested loops/problem5.js
+++ nested loops/problem5.js
@@ -8,3 +8,15 @@ for(i=1;i<=10;i++)
             }
      console.log(pattern)
     }
+
+
+ // 2nd
+ 
+ for(x=1;x<=8;x++){
+    let y=""
+    for(z=1;z<=x;z++)
+        {
+            y=y+x+" "
+        }
+        console.log(y)
+ }
---
+++ nested loops/problem6.js
@@ -0,0 +1,22 @@
+/*
+code for this type pattern
+
+ *****
+ ****
+ ***
+ **
+ *
+ 
+
+*/
+
+
+for(i=1;i<=5;i++)
+    {
+        let k=""
+        for(j=5;j>=i;j--)
+            {
+                k=k+"*"
+            }
+            console.log(k)
+    }
