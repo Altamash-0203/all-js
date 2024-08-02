b6c5542e658a2a2f6acf33004c28d806c2a7334f
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-02 15:11:41 +0530

    nested loop example

---
+++ nested loops/problem8.js
@@ -0,0 +1,13 @@
+for(i=1;i<=10;i++)
+    {
+        console.log("----loop start----")
+        for(j=1;j<=10;j++)
+            {
+              if(j>i)
+                {
+                    break
+                }
+                console.log("i is",i,"  ","j is",j)
+            }
+            console.log("----loop end----")
+    }
