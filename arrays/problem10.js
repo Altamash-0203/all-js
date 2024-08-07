19a77bc465e38faf0e35c099dbd9f812b1badcd1
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-07 10:40:13 +0530

    arrays

---
+++ arrays/problem10.js
@@ -0,0 +1,9 @@
+// print array reverse
+
+
+let arr=[10,20,30,40,50,60,70,80,90]
+
+for(i=arr.length-1;i>=0;i--)
+    {
+        console.log(arr[i])
+    }
---
+++ arrays/problem9.js
@@ -0,0 +1,11 @@
+// creat an array 
+// push 5 movies 
+// and run using loop
+
+let cinema=[]
+cinema.push('pk','race','don','king','manjumel boys')
+
+for(i=0;i<cinema.length;i++)
+    {
+        console.log(cinema[i]);
+    }
