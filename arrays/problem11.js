0b1cef775264e43d540760f45a9f122859223129
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-07 10:55:35 +0530

    arrays

---
+++ arrays/problem11.js
@@ -0,0 +1,24 @@
+// creat an array 
+// delet last three digit 
+// and print array with loop
+
+
+let arr=[1,2,3,4,5,6]
+
+
+// // first way
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// console.log(arr);
+
+
+//second way 
+
+for(i=0;i<=2;i++)
+    {
+        arr.pop()
+    }
+
+    console.log(arr)
+
---
+++ arrays/problem12.js
@@ -0,0 +1,23 @@
+//print first three index using loop
+
+
+let arr=["hell0","hi","bye","goodbye","hey",]
+
+//first way
+
+for(i=0;i<3;i++)
+    {
+        console.log(arr[i])
+    }
+
+
+ //second way 
+ 
+ for(i=0;i<=arr.length;i++)
+    {
+        if(i==3)
+            {
+                break;
+            }
+            console.log(arr[i])
+    }
