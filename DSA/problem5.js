dba009d576179aa0a1b1a54af4cb7a289264a757
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-08 23:17:22 +0530

    Strings

---
+++ DSA/problem5.js
@@ -0,0 +1,25 @@
+//
+
+
+
+
+
+
+let x=prompt("enter num")
+let bil=0
+if(x<=50)
+    {
+        bil=x*3
+       
+    }
+    else if(x<=100)
+        {
+            bil=bil+(x*5)
+            
+        }
+
+        else (x>150)
+        {
+            bil=bil+(x*10)
+            
+        }
