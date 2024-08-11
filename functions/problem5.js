4caec1a7d070c25f57354f7e6db2f39669c2e89b
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-11 10:50:29 +0530

    functions problem

--- functions/problem5.js
+++ functions/problem5.js
@@ -1,23 +1,22 @@
 // write a function to detemine num is prime or not
 
-function Checkprime(num) {
-    let factor = 0;
-
-    for (let i = 1; i <= num; i++) {
-        if (num % i == 0) {
-            factor++;
+function Checkprime(num)
+{
+    var factor=0
+    for(i=1;i<=num;i++)
+        {
+           if(num%i==0)
+            {
+                factor++;
+            }
         }
-    }
-
-    if (factor == 2) {
-        return true;
-    } else {
-        return false;
-    }
+        if(factor==2)
+            {
+            console.log(   "prime")
+            }
+            else{
+                 console.log("not prime");
+            }
 }
 
-if (Checkprime(11)) {
-    console.log("It's prime");
-} else {
-    console.log("Not prime");
-}
+Checkprime(11)
