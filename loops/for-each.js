d658f2474c4fc02fa63043e828ba2539f4172de9
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-21 15:53:59 +0530

    for-each loop

---
+++ loops/for-each.js
@@ -0,0 +1,22 @@
+//for each loop example 
+
+let arr =[1,2,3,4,5,6,7,8,9,10]
+let copy=[]
+
+    arr.forEach(function(para){
+        console.log(para*5)
+    })
+        
+
+    
+
+// example
+
+let arr1=[1,2,3,4,5]
+let arr2=[]
+
+arr1.forEach(function(index){
+    arr2.push(index)
+})
+    console.log(arr2)
+ 
