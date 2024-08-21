8038ea4e716764714682ecba21b5db78d2e7e4cb
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-08-21 10:15:08 +0530

    for-in and for of

---
+++ loops/for-in.js
@@ -0,0 +1,34 @@
+// for in loop in object
+
+let obj={
+    name:"shaikh",
+    contact:"mr.shaikh.altamash0203@gmail.com",
+    Hobbies:"cricket"
+
+}
+
+for(let key in obj)
+    {
+        console.log(obj[key])
+    }
+
+
+//for-in loop in array
+ 
+let arr=[10,20,30,40,50]
+
+for(let index in arr)
+    {
+        console.log(arr[index])
+    }
+
+
+
+//for in loop for string
+
+let myString="Altamash"
+ 
+for(let char in myString)
+    {
+        console.log(myString[char])
+    }
---
+++ loops/for-of.js
@@ -0,0 +1,19 @@
+// for of loop 
+
+//for of loop with array
+
+let arr=["name:shaikh","contact:982724842","work:developer"]
+for(let index of arr)
+    {
+        console.log(index)
+    }
+
+
+// for of loop with string
+
+let str="developer is majdoor"
+
+for(let  char of str)
+    {
+        console.log(char);
+    }
